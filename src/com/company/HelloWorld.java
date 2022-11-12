bdd1d4b6fddbcb89bb67655780226045e617935d
gfCrova <giancrova.cl5@gmail.com>
2022-11-11 22:01:08 -0300

    add new commit for practique

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -7,21 +7,21 @@ public class HelloWorld {
         System.out.println("Hello World"); // commentary
 
         // Function que return un text concat
-        System.out.println(printName2(cosa));
+        System.out.println(printName2(something));
         // Call a Function que no return nada
         printMyName(name2);
 
     }
     static String name2 = "Gustavo";
-    static String cosa = "Esto es un texto";
+    static String something = "This is a text";
 
     // FUNCTION QUE NO RETURN NADA (VOID)
     public static void printMyName(String nam) {
         System.out.println(nam);
     }
 
-    // FUNCTION QUE RETURN UN TIPO DE DATE
+    // FUNCTION QUE RETURN
     public static String printName2(String algo) {
-        return algo + " concatenado.";
+        return algo + " concatenate.";
     }
 }
