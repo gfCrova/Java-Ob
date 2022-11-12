a809d4aacdf36180d06fa7fcc2f7f7dda4772fae
gfCrova <giancrova.cl5@gmail.com>
2022-11-12 16:07:40 -0300

    if/else

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -4,16 +4,24 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World"); // commentary
+        String name2 = "Gustavo";
+        String something = "This is a text";
+        boolean check = true;
 
         // Function que return un text concat
         System.out.println(printName2(something));
         // Call a Function que no return nada
-        printMyName(name2);
+        printMyName(name2 + check);
 
+        if(number1 < number2 && number2 < number3){
+            System.out.println("Es menor");
+        } else if(number3 < number2){
+            System.out.println("No es menor");
+        } else {
+            System.out.println("Nada");
+        }
     }
-    static String name2 = "Gustavo";
-    static String something = "This is a text";
+
 
     // FUNCTION QUE NO RETURN NADA (VOID)
     public static void printMyName(String nam) {
@@ -24,4 +32,11 @@ public class HelloWorld {
     public static String printName2(String algo) {
         return algo + " concatenate.";
     }
+
+
+    static int number1 = 10;
+    static int number2 = 20;
+    static int number3 = 30;
+
+
 }
