d2ffcfd74c2fd0bc1c7415c950303681ba959475
gfCrova <giancrova.cl5@gmail.com>
2022-11-12 20:52:45 -0300

    update concepts for java

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -5,38 +5,38 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         String name2 = "Gustavo";
-        String something = "This is a text";
         boolean check = true;
 
-        // Function que return un text concat
-        System.out.println(printName2(something));
         // Call a Function que no return nada
         printMyName(name2 + check);
 
-        if(number1 < number2 && number2 < number3){
-            System.out.println("Es menor");
-        } else if(number3 < number2){
-            System.out.println("No es menor");
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+        if (number1 == number2 && number2 < number3) {
+            System.out.println("Todo Coincide");
+        } else if (number3 > number2) {
+            System.out.println("Number3 es mayor a Number2");
         } else {
             System.out.println("Nada");
         }
+
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Contador en: " + count);
+            count++;
+        }
     }
 
+        // FUNCTION QUE NO RETURN NADA (VOID)
+        public static void printMyName (String nam){
+            System.out.println(nam);
+        }
 
-    // FUNCTION QUE NO RETURN NADA (VOID)
-    public static void printMyName(String nam) {
-        System.out.println(nam);
-    }
-
-    // FUNCTION QUE RETURN
-    public static String printName2(String algo) {
-        return algo + " concatenate.";
-    }
-
-
-    static int number1 = 10;
-    static int number2 = 20;
-    static int number3 = 30;
+        // FUNCTION QUE RETURN
+        public static String printName2 (String algo){
+            return algo + " concatenate.";
+        }
 
 
 }
