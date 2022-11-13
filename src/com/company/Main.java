0ace0244b096fae4337a7ec314224fb7255d9443
gfCrova <giancrova.cl5@gmail.com>
2022-11-13 02:09:36 -0300

    working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Auto auto1 = new Auto("red", "Honda", "civic", 1460.60, 5.0);
-
         auto1.acelerar(80);
-
         System.out.println(auto1);
+
+
+        AutoElectrico autoElectrico1 = new AutoElectrico();
+        autoElectrico1.motorElectrico = "Motor 1.1";
+        autoElectrico1.modelo = "Honda";
+        autoElectrico1.color = "Azul";
+        autoElectrico1.peso = 1600.00;
+        autoElectrico1.largo = 5.0;
+        autoElectrico1.fabricante = "civic";
+        autoElectrico1.acelerar(100);
+        System.out.println(autoElectrico1);
     }
 }
