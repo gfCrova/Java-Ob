a1d2f75bb4416f0505365c3395b0c48af259642f
gfCrova <giancrova.cl5@gmail.com>
2022-11-28 23:23:02 -0300

    ejercicio terminado tema 1

--- src/com/ejercicioTEMA1/Main.java
+++ src/com/ejercicioTEMA1/Main.java
@@ -9,14 +9,21 @@ public class Main {
         short var2 = 15;
         int var3 = 140;
         long var4 = 5500000;
-        System.out.println(var1 + "," + var2 + "," + var3 + "," + var4);
+        System.out.println("Tipo byte: " + var1 + "\nTipo short: " + var2 + "\nTipo int: " + var3 + "\nTipo long: " + var4);
 
         // Decimals
         float var5 = 1.5f;
         double var6 = 26.02d;
-        System.out.println(var5 + var6);
+        System.out.println("Tipo float: " + var5 + "\nTipo double: " + var6);
 
         // Boolean
+        boolean var7 = true;
+        boolean var8 = false;
+        System.out.println("Tipo boolean: " + var7 + "\nTipo boolean: " + var8);
 
+        // String
+        char var9 = 'A';
+        String var10 = "Hola Mundo";
+        System.out.println("Tipo char: " + var9 + "\nTipo string: " + var10 );
     }
 }
