f2630dd32abf9ec4c9df85a5deeb6bc15b960a33
gfCrova <giancrova.cl5@gmail.com>
2022-11-28 23:14:00 -0300

    tipo de datos1

--- src/com/ejercicioTEMA1/Main.java
+++ src/com/ejercicioTEMA1/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // Integers
+        byte var1 = 2;
+        short var2 = 15;
+        int var3 = 140;
+        long var4 = 5500000;
+        System.out.println(var1 + "," + var2 + "," + var3 + "," + var4);
+
+        // Decimals
+        float var5 = 1.5f;
+        double var6 = 26.02d;
+        System.out.println(var5 + var6);
+
+        // Boolean
+
     }
 }
