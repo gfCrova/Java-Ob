2d8733d97af2c69aa5bd536de85db9f7d7efddf4
gfCrova <giancrova.cl5@gmail.com>
2022-12-12 21:18:20 -0300

    autor fixme

--- src/com/ejercicioTEMA3/Main.java
+++ src/com/ejercicioTEMA3/Main.java
@@ -1,9 +1,15 @@
 package com.ejercicioTEMA3;
+import java.util.Arrays;
 
+/**
+ * @author -Gian-
+ * @// FIXME: 12/12/2022
+ */
 public class Main {
     public static void main(String[] args) {
 
         String[] marcasDeCoches = {"Ferrari", "Chevrolet", "Ford", "Honda", "Toyota", "Tesla"};
+        System.out.println("\n" + Arrays.toString(marcasDeCoches) + "\n");
 
         int count = 1;
         for (String marca : marcasDeCoches) {
