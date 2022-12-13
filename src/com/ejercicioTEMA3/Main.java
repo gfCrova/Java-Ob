782c9dfe1e9b17e60a943e8d8e2c16f89dcb292e
gfCrova <giancrova.cl5@gmail.com>
2022-12-12 21:02:17 -0300

    modified code

--- src/com/ejercicioTEMA3/Main.java
+++ src/com/ejercicioTEMA3/Main.java
@@ -6,8 +6,8 @@ public class Main {
         String[] marcasDeCoches = {"Ferrari", "Chevrolet", "Ford", "Honda", "Toyota", "Tesla"};
 
         int count = 1;
-        for (String marcas:marcasDeCoches) {
-            String nuevaLista = count++ + " - " + marcas;
+        for (String marca : marcasDeCoches) {
+            String nuevaLista = count++ + " - " + marca;
             System.out.println(nuevaLista);
         }
     }
