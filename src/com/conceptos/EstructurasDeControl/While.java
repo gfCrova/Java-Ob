9afb662f0fcb28c0b66a551f22ad8fc7c969eb85
gfCrova <giancrova.cl5@gmail.com>
2022-12-16 20:21:35 -0300

    adding While

--- src/com/conceptos/EstructurasDeControl/While.java
+++ src/com/conceptos/EstructurasDeControl/While.java
@@ -8,6 +8,7 @@ public class While {
         int number1 = 10;
         int number2 = 20;
         int number3 = 30;
+
         if (number1 == number2 && number2 < number3) {
             System.out.println("Todo Coincide");
         } else if (number3 > number2) {
