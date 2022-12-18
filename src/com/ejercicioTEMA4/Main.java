42cb9995d2633ebd9e87e94d04af3cd2ffd47a18
gfCrova <giancrova.cl5@gmail.com>
2022-12-18 00:29:30 -0300

    se creo una nueva instancia de SmartPhone

--- src/com/ejercicioTEMA4/Main.java
+++ src/com/ejercicioTEMA4/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         SmartPhone smartPhone1 = new SmartPhone("Apple", "IPhone 13 Pro Max", 2021, "Plateado", "IOS 15", 6.7, "Wi-Fi", 6, 256,12, 10, true);
-        SmartWatch smartWatch1 = new SmartWatch("Xiaomi", "Mi Smart Band 6", 2022, "Negro", "Android 10", 1.56, "Bluetooth", "Silicona", "Deportiva", true);
+        SmartPhone smartPhone2 = new SmartPhone("Samsung", "Galaxy S20 FE", 2020, "Negro", "Android 10", 6.5, "Wi-Fi", 6, 128, 32, 12, false);        SmartWatch smartWatch1 = new SmartWatch("Xiaomi", "Mi Smart Band 6", 2022, "Negro", "Android 10", 1.56, "Bluetooth", "Silicona", "Deportiva", true);
 
         System.out.println(smartPhone1);
+        System.out.println("\n");
+        System.out.println(smartPhone2);
         System.out.println("\n");
         System.out.println(smartWatch1);
     }
