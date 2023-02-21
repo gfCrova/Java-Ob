8c2a74fb958259982710f9e036920384994cf7fa
gfCrova <giancrova.cl5@gmail.com>
2023-02-20 21:46:14 -0300

    ArrayList ejercico terminado

--- src/com/ejercicioTEMA6/ArrayList_LinkedList.java
+++ src/com/ejercicioTEMA6/ArrayList_LinkedList.java
@@ -9,6 +9,7 @@ public class ArrayList_LinkedList {
     public static void main(String[] args) {
 
         // ArrayList
+
         ArrayList<String> listado = new ArrayList<String>();
 
         listado.add("Elemento1");
@@ -25,6 +26,7 @@ public class ArrayList_LinkedList {
         System.out.println("\n");
 
         // Convertir de ArrayList para LinkedList
+
         LinkedList<String> listadoCopiado = new LinkedList<String>(listado);
 
         for (String elemento:listadoCopiado) {
@@ -34,11 +36,33 @@ public class ArrayList_LinkedList {
         System.out.println("\n");
 
         // Convertir de ArrayList para Array
+
         String[] array = new String[listado.size()];
         for (int i = 0; i < listado.size(); i++) {
             array[i] = listado.get(i);
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array) + "\n");
+
+        // ArrayList de Enteros
+
+        ArrayList<Integer> llenarElArray = new ArrayList<Integer>();
+
+        for (int i = 0; llenarElArray.size() < 10; i++) {
+            llenarElArray.add(1 + llenarElArray.size());
+        }
+        System.out.println(llenarElArray + "\n");
+
+        // Eliminar números pares del ArrayList de Enteros
+
+        for (int i = 0; i < llenarElArray.size(); i++) {
+            if (llenarElArray.get(i) % 2 == 0) {
+                llenarElArray.remove(i);
+            }
+        }
+
+        for (Integer num:llenarElArray) {
+            System.out.println(num);
+        }
     }
 
 }
