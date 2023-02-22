08badd1a4a8d0287ede40c1216784a7dcceafdfc
gfCrova <giancrova.cl5@gmail.com>
2023-02-21 22:50:46 -0300

    importando java.io correspondientes

--- src/com/ejercicioTEMA6/InputStream_PrintStream.java
+++ src/com/ejercicioTEMA6/InputStream_PrintStream.java
@@ -1,6 +1,10 @@
 package com.ejercicioTEMA6;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class InputStream_PrintStream {
 
