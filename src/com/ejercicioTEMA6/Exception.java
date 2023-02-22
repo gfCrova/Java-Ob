bbff7907e8e3b730085b9629fe4d84289da3cbef
gfCrova <giancrova.cl5@gmail.com>
2023-02-21 22:43:06 -0300

    generando io.exception

--- src/com/ejercicioTEMA6/Exception.java
+++ src/com/ejercicioTEMA6/Exception.java
@@ -2,16 +2,21 @@ package com.ejercicioTEMA6;
 
 import java.lang.ArithmeticException;
 
-public class ExceptionDividePorCero {
+public class Exception {
 
     public static void main(String[] args) {
 
-        System.out.println(DividePorCero(4, 0));
-
+        try {
+            System.out.println(DividePorCero(4, 0));
+        } catch (ArithmeticException e) {
+            System.out.println("Esto no puede hacerse");
+        } finally {
+            System.out.println("Demo de código");
+        }
     }
 
     public static int DividePorCero(int num1, int num2) throws ArithmeticException {
-        int resultado = 0;
+        int resultado;
         try {
             return resultado = num1 / num2;
         } catch (ArithmeticException e) {
---
+++ src/com/ejercicioTEMA6/InputStream_PrintStream.java
@@ -0,0 +1,29 @@
+package com.ejercicioTEMA6;
+
+import java.io.*;
+
+public class InputStream_PrintStream {
+
+    public static void main(String[] args) {
+
+        try {
+            copiaDeFicheros("C:\\Program Files\\WinRAR\\Rar.txt", "copia.txt");
+        } catch (IOException e) {
+            System.out.println("No se puede copiar el fichero");
+        }
+    }
+
+    public static void copiaDeFicheros(String fileIn, String fileOut) throws IOException {
+        try {
+            InputStream in = new FileInputStream(fileIn);
+            byte[] datos = in.readAllBytes();
+
+            PrintStream out = new PrintStream(fileOut);
+            out.write(datos);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+}
